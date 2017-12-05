7fe36c3dae8012a8f9719bb4c8a379892eaedf65
nickbrowne <nickbrowne@users.noreply.github.com>
2017-12-05 23:31:29 +1100

    Rename variable

--- examples/svg_to_png.rs
+++ examples/svg_to_png.rs
@@ -8,15 +8,15 @@ fn main() {
   let svg = nsvg::parse_file("examples/spiral.svg", "px", 72.0);
 
   // Rasterize the loaded SVG and return an RgbaImage
-  let image_buffer = nsvg::rasterize(svg);
+  let image = nsvg::rasterize(svg);
 
   let save_path = env::current_dir().unwrap().join("spiral_output.png");
-  let (width, height) = image_buffer.dimensions();
+  let (width, height) = image.dimensions();
 
   // Write the image to disk as a PNG
   image::save_buffer(
     save_path,
-    &image_buffer.into_raw(),
+    &image.into_raw(),
     width,
     height,
     image::ColorType::RGBA(8),
