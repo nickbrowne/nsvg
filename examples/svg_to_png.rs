41a7c52fcd9bc35fbff55960f7112efac489228f
Oliver Hamlet <oliver.hamlet@gmail.com>
2018-04-07 22:48:43 +0100

    Improve safety and idiomaticity of API
    - Fix memory leaks due to NanoSVG memory never getting freed.
    - Remove unwrap() usage outside tests, propagate errors to the caller.
     - Add a Units enum to safely represent the allowed values for the units
      parameter.
    - Expose methods to get SVG image width and height.

--- examples/svg_to_png.rs
+++ examples/svg_to_png.rs
@@ -5,10 +5,10 @@ use std::env;
 
 fn main() {
   // Load the SVG data
-  let svg = nsvg::parse_file("examples/spiral.svg", "px", 96.0);
+  let svg = nsvg::parse_file("examples/spiral.svg", nsvg::Units::Pixel, 96.0).unwrap();
 
   // Rasterize the loaded SVG and return an RgbaImage
-  let image = nsvg::rasterize(svg, 2.0);
+  let image = svg.rasterize(2.0).unwrap();
 
   let save_path = env::current_dir().unwrap().join("spiral_output.png");
   let (width, height) = image.dimensions();
